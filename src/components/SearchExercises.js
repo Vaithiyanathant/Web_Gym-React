545df250c88dfe69ae9fd2102423e8cc8d310ba4
vaithiyanathan <vaithiyanathan30@gmail.com>
2023-12-16 05:23:43 +0530

    api not working for more data and produces less data API INCOMPLETE

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,72 +1,104 @@
 /** @format */
 
-import React, { useState, useEffect } from "react";
-import {
-	Box,
-	Button,
-	Stack,
-	TextField,
-	Typography,
-	typography,
-} from "@mui/material";
-import { exerciseOptions, fetchData } from "../utils/fetchData";
+import React, { useEffect, useState } from "react";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 
-export default function SearchExercises() {
-	const [search, setsearch] = useState("");
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+import HorizontalScrollbar from "./HorizontalScrollbar";
+
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
+	const [search, setSearch] = useState("");
+	const [bodyParts, setBodyParts] = useState([]);
+
+	useEffect(() => {
+		const fetchExercisesData = async () => {
+			const bodyPartsData = await fetchData(
+				"https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+				exerciseOptions
+			);
+
+			setBodyParts(["all", ...bodyPartsData]);
+		};
+
+		fetchExercisesData();
+	}, []);
 
 	const handleSearch = async () => {
 		if (search) {
-			const exercisesDate = await fetchData(
-				'https://exercisedb.p.rapidapi.com/exercises',
+			const exercisesData = await fetchData(
+				"https://exercisedb.p.rapidapi.com/exercises",
 				exerciseOptions
 			);
-			console.log(exercisesDate);
+			const searchedExercises = exercisesData.filter(
+				(item) =>
+					item.name.toLowerCase().includes(search) ||
+					item.target.toLowerCase().includes(search) ||
+					item.equipment.toLowerCase().includes(search) ||
+					item.bodyPart.toLowerCase().includes(search)
+			);
+
+			window.scrollTo({ top: 1800, left: 100, behavior: "smooth" });
+
+			setSearch("");
+			setExercises(searchedExercises);
 		}
 	};
 
 	return (
 		<Stack
 			alignItems='center'
-			mt='40px'
+			mt='37px'
 			justifyContent='center'
 			p='20px'>
 			<Typography
 				fontWeight={700}
 				sx={{ fontSize: { lg: "44px", xs: "30px" } }}
-				mb='50px'
-				textAlign='center'
-				mt='100px'>
-				Awesome Exercises You should know
+				mb='49px'
+				textAlign='center'>
+				Awesome Exercises You <br /> Should Know
 			</Typography>
 			<Box
 				position='relative'
 				mb='72px'>
 				<TextField
+					height='76px'
 					sx={{
-						input: { fontWeight: "700", border: "none", borderRadius: "40px" },
+						input: { fontWeight: "700", border: "none", borderRadius: "4px" },
 						width: { lg: "1170px", xs: "350px" },
+						backgroundColor: "#fff",
+						borderRadius: "40px",
 					}}
-					height='72px'
 					value={search}
-					onChange={(e) => setsearch(e.target.value.toLowerCase())}
+					onChange={(e) => setSearch(e.target.value.toLowerCase())}
 					placeholder='Search Exercises'
-					type='text'></TextField>
+					type='text'
+				/>
 				<Button
+					className='search-btn'
 					sx={{
 						bgcolor: "#FF2625",
 						color: "#fff",
 						textTransform: "none",
-						width: { lg: "175px", xs: "80px" },
-						fontSize: { lg: "20px", xs: "10px" },
+						width: { lg: "173px", xs: "80px" },
 						height: "56px",
 						position: "absolute",
-						right: "0",
+						right: "0px",
+						fontSize: { lg: "20px", xs: "14px" },
 					}}
-					className='Search-btn'
-					onClick={handleSearch()}>
+					onClick={handleSearch}>
 					Search
 				</Button>
 			</Box>
+			<Box sx={{ position: "relative", width: "100%", p: "20px" }}>
+				<HorizontalScrollbar
+					data={bodyParts}
+					bodyParts
+					setBodyPart={setBodyPart}
+					bodyPart={bodyPart}
+				/>
+			</Box>
 		</Stack>
 	);
-}
+};
+
+export default SearchExercises;
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -5,11 +5,12 @@ export const exerciseOptions = {
 	headers: {
 		"X-RapidAPI-Key": "741b23ce23msh575ac8ac97f2173p14385ejsncc30b8463cf0",
 		"X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-	},
+	}
 };
 
 export const fetchData = async (url, options) => {
 	const response = await fetch(url, options);
 	const data = await response.json();
+
 	return data;
 };
